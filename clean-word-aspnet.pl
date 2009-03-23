cc6fea4cb61bc3ed33d820c43cbb9e7b888e1425
Tim Abell <tim@emapsite.com>
2009-03-23 16:08:49 +0000

    applied gpl v3

--- clean-word-aspnet.pl
+++ clean-word-aspnet.pl
@@ -1,4 +1,21 @@
 #!/usr/bin/perl
+
+#  Script for cleaning word cruft from asp.net files
+#  Copyright (C) 2009 Tim Abell
+
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 use strict;
 use warnings;
 use Data::Dumper; #for debug output
@@ -9,6 +26,7 @@ use HTML::Parser; #for empty span remover
 #package WordCleaner;
 
 print "Script for cleaning word cruft from asp.net files\n";
+print "Licensed under GPL v3. See COPYING for full licence.\n";
 print "Tim Abell 2009\n";
 
 # get arguments
